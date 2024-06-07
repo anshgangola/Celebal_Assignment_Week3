34ff4f6e7042190e84407cca5e934278ffa2dcf6
anshgangola <105276872+anshgangola@users.noreply.github.com>
2024-06-08 03:08:05 +0530

    First Commit
    Added all the files

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css"; // Ensure this import is present
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Admin Dashboard</h1>
+      <div className="theme-switcher">
+        {/* Your theme switcher component or toggle button */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
